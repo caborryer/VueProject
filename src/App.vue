660e2755cd6d48822443e6535ce0118fcb6144a9
caborryer <corraleshyulia@gmail.com>
2020-01-09 17:18:20 -0500

    all

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <Add-Todo/>
+      <Filter-Todos/>
+      <Todos />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos.vue";
+import AddTodo from './components/AddTodo.vue'
+import FilterTodos from './components/FilterTodos.vue'
+
+export default {
+  name: "app",
+  components: {
+    Todos,
+    AddTodo,
+    FilterTodos
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif,
+    Arial, sans-serif;
+  line-height: 1.6;
+  background: beige;
+}
+
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import Vuex from "vuex";
+import Vue from "vue";
+import todos from "./modules/todos";
+
+//Load vuex
+Vue.use(Vuex);
+
+//Create store
+export default new Vuex.Store({ modules: { todos } });
